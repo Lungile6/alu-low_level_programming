35abe67ab02c6d5a9831377aac00616ce22003c9
Lungile6 <l.mabelebel@alustudent.com>
2025-06-02 06:27:06 -0700

    First Trial

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer.
+ * @n: number to check
+ *Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	_putchar('\n');
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *times_table - prints the 9 times table, starting with 0.
+ *
+ *Return:
+ */
+void times_table(void)
+{
+	int row;
+	int col;
+	int result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			result = (row * col);
+
+			if (col == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
